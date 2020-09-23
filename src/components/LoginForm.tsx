ef1f693c66f560e3b364307b41bc63106360f075
josh richards <joshuarichards001@gmail.com>
2020-09-23 14:23:08 +1200

    Restyled login buttons

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,11 +1,11 @@
 import React, { ReactElement, useState } from "react";
 import styles from "../Main.module.css";
-import GoogleLogin from 'react-google-login';
+import GoogleLogin from "react-google-login";
 
 interface Props {
   loading: boolean;
   onLoginClick: (userName: string, password: string) => void | undefined;
-  onGoogleClick: (response: any) => Promise<void>
+  onGoogleClick: (response: any) => Promise<void>;
   onRegisterClick: (
     email: string,
     userName: string,
@@ -58,41 +58,44 @@ export default function LoginForm({
             style={{ marginBottom: "40px" }}
             type="password"
           />
+          <div style={{display: "flex", justifyContent: "space-between"}}>
+            <button
+              onClick={(e) => {
+                if (!showRegister) {
+                  e.preventDefault();
+                  onLoginClick(userName, password);
+                } else {
+                  setShowRegister(false);
+                }
+              }}
+              className={styles.buttonStyle}
+              style={{ backgroundColor: "#e0e0e0" }}
+            >
+              Login
+            </button>
+            <button
+              className={styles.buttonStyle}
+              style={{ backgroundColor: "#e0e0e0" }}
+              onClick={(e) => {
+                if (
+                  (!showRegister || email.length,
+                  userName.length,
+                  password.length === 0)
+                ) {
+                  e.preventDefault();
+                  setShowRegister(true);
+                } else {
+                  e.preventDefault();
+                  onRegisterClick(email, userName, password);
+                }
+              }}
+            >
+              Register
+            </button>
+          </div>
           <button
-            onClick={(e) => {
-              if (!showRegister) {
-                e.preventDefault();
-                onLoginClick(userName, password);
-              } else {
-                setShowRegister(false);
-              }
-            }}
             className={styles.buttonStyle}
-            style={{ backgroundColor: "#aaaaaa" }}
-          >
-            Login
-          </button>
-          <button
-            className={styles.buttonStyle}
-            onClick={(e) => {
-              if (
-                (!showRegister || email.length,
-                userName.length,
-                password.length === 0)
-              ) {
-                e.preventDefault();
-                setShowRegister(true);
-              } else {
-                e.preventDefault();
-                onRegisterClick(email, userName, password);
-              }
-            }}
-          >
-            Register
-          </button>
-          <button
-            className={styles.buttonStyle}
-            style={{ backgroundColor: "#eeeeee" }}
+            style={{ backgroundColor: "#e0e0e0" }}
             onClick={(e) => {
               e.preventDefault();
               onLoginClick("testUser", "testUserPass");
@@ -102,22 +105,47 @@ export default function LoginForm({
           </button>
           <GoogleLogin
             clientId="551797952328-07qc03g60octqej7u462de4vbgtfkbm9.apps.googleusercontent.com"
+            render={(renderProps) => (
+              <button
+                onClick={renderProps.onClick}
+                disabled={renderProps.disabled}
+                className={styles.buttonStyle}
+                style={{ backgroundColor: "#e0e0e0" }}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="26"
+                  viewBox="0 0 24 24"
+                  style={{marginRight: "10px", marginBottom: "1px"}}
+                >
+                  <path
+                    d="M7 11v2.4h3.97c-.16 1.029-1.2 3.02-3.97 3.02-2.39 0-4.34-1.979-4.34-4.42 0-2.44 1.95-4.42 4.34-4.42 1.36 0 2.27.58 2.79 1.08l1.9-1.83c-1.22-1.14-2.8-1.83-4.69-1.83-3.87 0-7 3.13-7 7s3.13 7 7 7c4.04 0 6.721-2.84 6.721-6.84 0-.46-.051-.81-.111-1.16h-6.61zm0 0 17 2h-3v3h-2v-3h-3v-2h3v-3h2v3h3v2z"
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+                Sign in with Google
+              </button>
+            )}
             buttonText="Login with Google"
             onSuccess={onGoogleClick}
             onFailure={onGoogleClick}
           />
           {loading ? (
-            <p style={{ marginTop: "10px" }}>Content Is Loading, this may take a minute...</p>
+            <p style={{ marginTop: "10px" }}>
+              Content Is Loading, this may take a minute...
+            </p>
           ) : null}
         </form>
-        <div className={styles.loginForm} style={{width: "300px"}}>
+        <div className={styles.loginForm} style={{ width: "300px" }}>
           <h1>About</h1>
           <p style={{ marginTop: "20px" }}>
-            A budgeting app that was built to develop my skills in 
-            full-stack web development. It is built using:
+            A budgeting app that was built to develop my skills in full-stack
+            web development. It is built using:
           </p>
           <div style={{ marginTop: "20px", display: "flex" }}>
-            <div style={{ marginRight: "50px"}}>
+            <div style={{ marginRight: "50px" }}>
               <li>React</li>
               <li>TypeScript</li>
               <li>CSS</li>
@@ -131,9 +159,10 @@ export default function LoginForm({
             </div>
           </div>
           <p style={{ marginTop: "20px" }}>
-            You can either log in as a guest, register yourself, login with google, or use some existing dummy users like:
+            You can either log in as a guest, register yourself, login with
+            google, or use some existing dummy users like:
           </p>
-          <table style={{ marginTop: "20px", width: "100px" }}>   
+          <table style={{ marginTop: "20px", width: "100px" }}>
             <thead>
               <tr>
                 <th>Username</th>
